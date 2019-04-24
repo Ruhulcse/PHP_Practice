4f98e3511c1ecf4c4fc12e25bf726637d7d5dd13
Ruhul amin <ruhul.cse7862@gmail.com>
2019-04-24 21:30:27 +0600

    Create insert.php

---
+++ BanglaDB/insert.php
@@ -0,0 +1,11 @@
+ <?php
+ require 'db.php';
+ ?>
+<form action="store.php" method="POST">
+   <input required="" type="text" name="post"style="
+    height: 199px;
+    width: 283px;
+">
+  </br>
+   <button type="submit">Add data</button>
+ </form>
